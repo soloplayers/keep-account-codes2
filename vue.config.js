ec958409524664ef828b5f2ac67251ec0a17b4b6
soloplayers <937059391@qq.com>
2020-11-27 10:17:04 +0800

    finish beautify

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path=require('path');
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/keep-accounts-website/'
+    ? '/keep-account-website2/'
     : '/',
   lintOnSave: false,
   chainWebpack: (config)=>{
